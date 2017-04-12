7ddc16c0533858166261f493031bca6c28a7afa6
r0h <watusaproject@gmail.com>
2017-04-12 14:55:17 +0200

    s

--- exit/js/slider.js
+++ exit/js/slider.js
@@ -1,33 +1,46 @@
 var slideNum = 1;
 showSlides(slideNum);
 
+//Prev and Next function
 function plusSlide(n) {
 	showSlides(slideNum += n);
 }
+
+//Click small image
 function current(n) {
 	showSlides(slideNum = n);
 }
+
+//Main function moves slider
 function showSlides(n) {
 	var i,
 	slides = document.getElementsByClassName("slider"),
 	mini = document.getElementsByClassName("mini");
 
+	//Infinitive loop
 	if (n > slides.length) {
 		slideNum = 1
 	}
 	if (n < 1 ) {
 		slideNum = slides.length
 	}
+
+	//Hiding all slides
 	for (i=0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
+
+	//Taking off the "active" from small images
 	for (i=0; i < slides.length; i++) {
 		mini[i].className = mini[i].className.replace("active", "");
 	}
+
+	//Showing current slide and adding "active" to current small image
 	slides[slideNum -1].style.display = "block";
 	mini[slideNum -1].className+= " active";
 }
 
+//Adding touch for mobile
 var initialPoint;
 var finalPoint;
 document.addEventListener('touchstart', function(event) {
@@ -50,5 +63,3 @@ document.addEventListener('touchend', function(event) {
 				}
 			}
 		}, false);
-
-previous.addEventListener('touchstart', plusSlide(-1));
--- exit/js/slider_jquery.js
+++ exit/js/slider_jquery.js
@@ -1,4 +1,40 @@
-$(document).ready(function(){ 
+	var slideNum = 1;
+	showSlides(slideNum);
 
-	$(".slider").hide();
+	function plusSlide(n) {
+		showSlides(slideNum += n);
+	}
+	function current(n) {
+		showSlides(slideNum = n);
+	}
+	function showSlides(n) {
+
+		if (n > $(".slider").length) {
+			slideNum = 1
+		}
+		if (n < 1 ) {
+			slideNum = $(".slider").length
+		}
+		for (i=0; i < $(".slider").length; i++) {
+			$(".slider").css("display", "none");
+		}
+		for (i=0; i < $(".slider").length; i++) {
+			$(".mini")[i].className = $(".mini")[i].className.replace("active", "");
+		}
+		$(".slider")[slideNum -1].style.display = "block";
+		$(".mini")[slideNum -1].className+= " active";
+	}
+
+//Adding touch with "jquery.mobile"
+	$(".container").on("swipeleft",function(){
+		plusSlide(1);
+	});
+	$(".container").on("swiperight",function(){
+		plusSlide(-1);
+	});
+
+//Removing unliked effects after adding "jquery.mobile"
+$(document).ready(function(){	
+	$(".ui-loader").hide();
+	$(".ui-page").removeAttr("tabindex");
 });
